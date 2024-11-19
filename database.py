13155217cf49f83a90453987be287c27871abf74
Mirekczk <35351910+mirekczk@users.noreply.github.com>
2024-11-19 15:25:29 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,187 +1,206 @@
-import sqlite3
-import json
+import datetime
+import re
+import os
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, field_validator, model_validator, Field
 
-db_file = "zalohaDB/LGE_2024.db"
-
-
-def create_connection(db_file=db_file):
-    conn = sqlite3.connect(db_file)
-    conn.cursor().execute('''
-                          CREATE TABLE IF NOT EXISTS ReservedSlots (
-                            id text PRIMARY KEY,
-                            slot_date text NOT NULL,
-                            slot_time text NOT NULL,
-                            customer_cap text NOT NULL,
-                            quantity integer NOT NULL,
-                            product_id integer NOT NULL);
-                          ''')
-
-    conn.cursor().execute('''
-                          CREATE TABLE IF NOT EXISTS AvailableSlots (
-                            date text PRIMARY KEY,
-                            count_odpol integer NOT NULL,
-                            count_dopol integer NOT NULL);
-                          ''')
-
-    return conn
+svatky = ["2024-04-01", "2024-05-01", "2024-05-08", "2024-07-05",
+          "2024-07-06", "2024-09-28", "2024-10-28", "2024-11-17", 
+          "2024-12-24", "2024-12-25", "2024-12-26", "2025-01-01",
+          "2025-04-18", "2025-04-21", "2025-05-01", "2025-05-08", 
+          "2025-07-05", "2025-07-06", "2025-09-28", "2025-10-28", 
+          "2025-11-17", "2025-12-24", "2025-12-25", "2025-12-26"]
 
 
-def select_as(datumOd, datumDo):
-    conn = create_connection()
-    conn.row_factory = lambda cursor, row: row[0]
-    cursor = conn.cursor()
-    cursor.execute(f'''
-                SELECT json_object(
-                    'date', date,
-                    'count_dopol', count_dopol,
-                    'count_odpol', count_odpol)
-                FROM AvailableSlots as2
-                WHERE  as2.date BETWEEN '{datumOd}' AND '{datumDo}'
-                ''')
-    rows = cursor.fetchall()
-    return [json.loads(x) for x in rows]
+class AvailableSlot(BaseModel):
+    date_start: str = Field(
+        pattern=r"^(202)\d{1}(\-)(0[1-9]|1[0,1,2])(\-)(0[1-9]|[12][0-9]|3[01])$", description="Date format YYYY-MM-DD")
+    date_stop: str = Field(
+        pattern=r"^(202)\d{1}(\-)(0[1-9]|1[0,1,2])(\-)(0[1-9]|[12][0-9]|3[01])$", description="Date format YYYY-MM-DD")
+    time_slots: str = Field(pattern=r"^(08-12)|(12-16)|(08-16)$",
+                            description="Timeslot format HH-HH. Only 08-12 and 12-16 and 08-16 are allowed.")
+    customer_zipcode: str
 
-    
-def select_availeble_slots_dopol(datumOd, datumDo):
-    conn = create_connection()
-    conn.row_factory = lambda cursor, row: row[0]
-    cursor = conn.cursor()
-    cursor.execute(f'''
-                SELECT json_object(
-                    'date', date,
-                    'time_slot', '08-12',
-                    'slot_number', count_dopol,
-                    'delivery_appointment_date',IIF(as2.count_dopol > 0, date||" 8:01", NULL))
-                FROM AvailableSlots as2
-                WHERE  as2.date BETWEEN '{datumOd}' AND '{datumDo}'
-                ''')
-    rows = cursor.fetchall()
-    return [json.loads(x) for x in rows]
+    @field_validator('customer_zipcode')
+    def validate_zipcode(cls, customer_zipcode: str):
+        if re.match("^[1-7]\\d{4}$|^([1-7]\\d{2} \\d{2})$", customer_zipcode):
+            pass
+        else:
+            raise HTTPException(
+                422, "ZIP code format has to be five digits. For example: 60200 or 602 00")
+        return customer_zipcode
 
-def select_availeble_slots_odpol(datumOd, datumDo):
-    conn = create_connection()
-    conn.row_factory = lambda cursor, row: row[0]
-    cursor = conn.cursor()
-    cursor.execute(f'''
-                SELECT json_object(
-                    'date', date,
-                    'time_slot', '12-16',
-                    'slot_number', count_odpol,
-                    'delivery_appointment_date',IIF(as2.count_odpol > 0, date||" 12:01", NULL))
-                FROM AvailableSlots as2
-                WHERE  as2.date BETWEEN '{datumOd}' AND '{datumDo}'
-                ''')
-    rows = cursor.fetchall()
-    return [json.loads(x) for x in rows]
+    @model_validator(mode="after")  # type: ignore
+    def validate_date(cls, values: "AvailableSlot"):
+        try:
+            start_date = datetime.date(int(values.date_start.split("-")[0]), int(
+                values.date_start.split("-")[1]), int(values.date_start.split("-")[2]))
+            end_date = datetime.date(int(values.date_stop.split("-")[0]), int(
+                values.date_stop.split("-")[1]), int(values.date_stop.split("-")[2]))
+            if end_date < start_date or start_date < datetime.date.today():
+                raise HTTPException(422, "Bad date. Use actual and in format YYYY-MM-DD.")
+        except ValueError:
+            raise HTTPException(422, "Bad date. Use actual and in format YYYY-MM-DD.")
+        except KeyError:
+            raise HTTPException(422, "Bad date. Use actual and in format YYYY-MM-DD.")
+        return values
 
-def select_availeble_slots_vse(datumOd, datumDo):
-    conn = create_connection()
-    conn.row_factory = lambda cursor, row: row[0]
-    cursor = conn.cursor()
-    cursor.execute(f'''
-                SELECT json_object(
-                    'date', date,
-                    'time_slot', '08-12',
-                    'slot_number', count_dopol,
-                    'delivery_appointment_date',IIF(as2.count_dopol > 0, date||" 8:01", NULL))
-                FROM AvailableSlots as2
-                WHERE  as2.date BETWEEN '{datumOd}' AND '{datumDo}'
-                UNION
-                SELECT json_object(
-                    'date', date,
-                    'time_slot', '12-16',
-                    'slot_number', count_odpol,
-                    'delivery_appointment_date',IIF(as2.count_odpol > 0, date||" 12:01", NULL))
-                FROM AvailableSlots as2 
-                WHERE  as2.date BETWEEN '{datumOd}' AND '{datumDo}'
-                ''')
-    rows = cursor.fetchall()
-    return [json.loads(x) for x in rows]
-
-def insert_slot(values):
-    # Establish a connection to the database
-    conn = create_connection()
-    cursor = conn.cursor()
-    if values != None:
-        cursor.execute(
-            'INSERT INTO ReservedSlots (id, slot_date, slot_time, customer_cap, quantity, product_id) VALUES(?,?,?,?,?,?)', values)
-        if values[2]=="08-12":
-            cursor.execute(f"UPDATE AvailableSlots SET count_dopol = count_dopol - 1 WHERE count_dopol > 0 AND AvailableSlots.date = '{values[1]}'")
-        elif values[2]=="12-16":
-            cursor.execute(f"UPDATE AvailableSlots SET count_odpol = count_odpol - 1 WHERE count_odpol > 0 AND AvailableSlots.date = '{values[1]}'")
-            
-    # Commit the changes and close the connection
-    conn.commit()
-    conn.close()
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "date_start": "2024-05-15",
+                    "date_stop": "2024-05-19",
+                    "time_slots": "08-16",
+                    "customer_zipcode": "602 00"
+                }
+            ]
+        }
+    }
 
 
-def edit_slots(values):
-    # Establish a connection to the database
-    conn = create_connection()
-    cursor = conn.cursor()
-    if values != None:
-        for value in values:
-            #print(value)
-            cursor.execute(f'INSERT OR REPLACE INTO AvailableSlots (date,count_dopol,count_odpol) VALUES(?,?,?)', value)
-    # Commit the changes and close the connection
-    conn.commit()
-    conn.close()
-    
-def edit_slots2(value):
-    # Establish a connection to the database
-    conn = create_connection()
-    cursor = conn.cursor()
-    cursor.execute(f'INSERT OR REPLACE INTO AvailableSlots (date,count_dopol,count_odpol) VALUES(?,?,?)', value)
-    # Commit the changes and close the connection
-    conn.commit()
-    conn.close()
+class DeliverySlot(BaseModel):
+    slot_date: str = Field(
+        pattern=r"^(202)\d{1}(\-)(0[1-9]|1[0,1,2])(\-)(0[1-9]|[12][0-9]|3[01])$", description="Date format YYYY-MM-DD")
+    slot_time: str = Field(
+        description="Timeslot in format HH-HH. For Delivery and instalation => 08-12 or 12-16. For only delivery => 08-16.")
+    customer_zipcode: str = Field(description="ZIP code. For example: 60200 or 602 00. CZ ZIPcodes 100 00 - 800 00")
+    document_no: str = Field(default="LGEPL_Number1_Number2_Number3",
+                             description="Interface Key Value. (Subsidiary_Number1_Number2_Number3)")
+    quantity: str | None = "1"
+    partner: str | None = "LGE_automatic"
 
-def delete_slot(id: str) -> str:
-    cursor = create_connection()
-    data = cursor.execute(f'SELECT slot_date, slot_time FROM ReservedSlots WHERE id="{id}"')
-    rows = data.fetchall()
-    print(rows[0][1])
-    if rows != []:
-        if rows[0][1] == "08-12":
-            cursor.execute(f"UPDATE AvailableSlots SET count_dopol = count_dopol + 1 WHERE AvailableSlots.date = '{rows[0][0]}'")
-        elif rows[0][1]=="12-16":
-            cursor.execute(f"UPDATE AvailableSlots SET count_odpol = count_odpol + 1 WHERE AvailableSlots.date = '{rows[0][0]}'")
-        cursor.execute(f'DELETE FROM ReservedSlots WHERE id="{id}"')
-    cursor.commit()
-    cursor.close()
-    return id
+    @field_validator('customer_zipcode')
+    def validate_zipcode(cls, customer_zipcode: str):
+        if re.match("^\\d{5}$|^(\\d{3} \\d{2})$", customer_zipcode):
+            pass
+        else:
+            raise HTTPException(
+                422, "ZIP code format has to be five digits. For example: 60200 or 602 00")
+        return customer_zipcode
 
-def number_of_rows(datum: str) -> int:
-    cursor = create_connection().cursor()
-    data = cursor.execute(
-        f'SELECT COUNT(slot_date) FROM ReservedSlots WHERE slot_date="{datum}"')
-    return data.fetchall()[0][0]
+    @field_validator('quantity')
+    def validate_quantity(cls, quantity: str):
+        if re.match("^[1-9][0-9]*$", quantity):
+            pass
+        else:
+            raise HTTPException(
+                422, "Quantity has to be positive number.")
+        return quantity
+
+    @field_validator('slot_time')
+    def validate_time_slot(cls, time_slot: str):
+        if re.match("^(08-12)|(12-16)|(08-16)$", time_slot):
+            pass
+        else:
+            raise HTTPException(
+                422, "Bad timeslot. Timeslot in format HH-HH. For Delivery and instalation => 08-12 or 12-16. For only delivery => 08-16.")
+        return time_slot
+
+    @model_validator(mode="after")  # type: ignore
+    def validate_date(cls, values: "DeliverySlot"):
+        try:
+            slot_date = datetime.date(int(values.slot_date.split("-")[0]), int(
+                values.slot_date.split("-")[1]), int(values.slot_date.split("-")[2]))
+            if slot_date < datetime.date.today():
+                raise HTTPException(
+                    422, "Bad date. Use actual and in format YYYY-MM-DD.")
+        except ValueError:
+            raise HTTPException(
+                422, "Bad date. Use actual and in format YYYY-MM-DD.")
+        except KeyError:
+            raise HTTPException(
+                422, "Bad date. Use actual and in format YYYY-MM-DD.")
+        return values
+
+    def get_branch_id(self):
+        if self.customer_zipcode != None:
+            zipcode = self.customer_zipcode.replace(' ', '')
+            zipcode = int(zipcode)
+            if (10000 <= zipcode <= 29999) or (37000 <= zipcode <= 49999) or (58000 <= zipcode <= 58999):
+                result = "PRA"
+            elif (56800 <= zipcode <= 57999) or (59000 <= zipcode <= 69999) or (75000 <= zipcode <= 75130) or (75136 <= zipcode <= 75299) or (76000 <= zipcode <= 79099) or (79600 <= zipcode <= 79999):
+                result = "BRN"
+            elif (70000 <= zipcode <= 74999) or (75131 <= zipcode <= 75135) or (75300 <= zipcode <= 75999) or (79100 <= zipcode <= 79599):
+                result = "OST"
+            elif (30000 <= zipcode <= 36999):
+                result = "PLZ"
+            elif (50000 <= zipcode <= 56799):
+                result = "HKR"
+            else:
+                result = "XXX"
+        else:
+            result = "XXX"
+        return result
+
+  
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {   "slot_date": "2024-12-18",
+                    "slot_time": "12-16",
+                    "document_no": "LGEXX_Number1_Number2_Number3",
+                    "customer_zipcode": "691 21",
+                    "quantity": "1"
+                 }
+            ]
+        }
+    }
 
 
-def get_avlslot_counts(datum: str) -> dict:
-    conn = create_connection()
-    conn.row_factory = lambda cursor, row: row[0]
-    cursor = conn.cursor()
-    data = cursor.execute(
-        f'''
-        SELECT
-        json_object(
-            'date', date, 
-            'count_dopol', count_dopol,
-            'count_odpol', count_odpol)
-        FROM AvailableSlots as2
-        WHERE date="{datum}"
-        ''')
-    rows = data.fetchall()
-    return json.loads(rows[0])
+class Login(BaseModel):
+    username: str
+    password: str
 
-def slot_exist(id: str) -> bool:
-    cursor = create_connection()
-    data = cursor.execute(
-        f'SELECT slot_date FROM ReservedSlots WHERE id="{id}"')
-    rows = data.fetchall()
-    if rows != []:
-        # print(rows[0][0])
-        return True
-    else:
-        return False
+    @model_validator(mode="after")  # type: ignore
+    def is_auth(cls, login: "Login"):
+        if (login.username == os.getenv('login_test') and login.password == os.getenv('password_test')) or (login.username == os.getenv('login_prod') and login.password == os.getenv('password_prod')):
+            return login
+        else:
+            raise HTTPException(
+                401, "Unauthorized. Wrong password or username.")
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "username": "lg.wsd",
+                    "password": "******************"
+                }
+            ]
+        }
+    }
+
+
+class GetAvailableSlots(BaseModel):
+    login: Login
+    get_available_slots: AvailableSlot
+
+    def get_branch_id(self):
+        if self.get_available_slots.customer_zipcode != None:
+            zipcode = self.get_available_slots.customer_zipcode.replace(
+                ' ', '')
+            zipcode = int(zipcode)
+            if (10000 <= zipcode <= 29999) or (37000 <= zipcode <= 49999) or (58000 <= zipcode <= 58999):
+                result = "PRA"
+            elif (56800 <= zipcode <= 57999) or (59000 <= zipcode <= 69999) or (75000 <= zipcode <= 75130) or (75136 <= zipcode <= 75299) or (76000 <= zipcode <= 79099) or (79600 <= zipcode <= 79999):
+                result = "BRN"
+            elif (70000 <= zipcode <= 74999) or (75131 <= zipcode <= 75135) or (75300 <= zipcode <= 75999) or (79100 <= zipcode <= 79599):
+                result = "OST"
+            elif (30000 <= zipcode <= 36999):
+                result = "PLZ"
+            elif (50000 <= zipcode <= 56799):
+                result = "HKR"
+            else:
+                result = "XXX"
+        else:
+            result = "XXX"
+        return result
+
+
+class ReserveDeliverySlot(BaseModel):
+    login: Login
+    reserve_slot: DeliverySlot
+
+class UpdateDocumentNumber(BaseModel):
+    update_document_no: str = Field(description="Interface Key Value. (Subsidiary_Number1_Number2_Number3)")
